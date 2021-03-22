bf8ff3239e1d51005a07cc51a5e5faee22a9f32e
Grant Galinger <galinggr@mail.uc.edu>
2021-03-22 12:47:20 -0400

    removed local dropzone.js file

--- Development/upload.php
+++ Development/upload.php
@@ -35,7 +35,8 @@
 		<div class="content">
 			<form action="includes/upload.inc.php" class="dropzone" id="myAwesomeDropzone"> </form>  
 			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-			<script src="js/dropzone.js" type="text/javascript"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.8.1/min/dropzone.min.js" type="text/javascript"></script>
+			<!-- <script src="js/dropzone.js" type="text/javascript"></script> -->
 		</div> 
 	</div>
 
